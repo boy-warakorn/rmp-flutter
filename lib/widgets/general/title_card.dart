6cc569cbd0ac8fd0dca396862c6ef112f317fc2f
warakorn <boydoubleb@gmail.com>
2021-09-11 14:24:00 +0700

    update: editing

--- lib/widgets/general/title_card.dart
+++ lib/widgets/general/title_card.dart
@@ -16,50 +16,32 @@ class TitleCard extends StatelessWidget {
     required this.count,
   }) : super(key: key);
 
-  Widget _buildTitle(context) {
-    return Text(
-      this.title,
-      style: Theme.of(context).textTheme.headline4,
-    );
-  }
-
-  Widget _buildCount(context) {
-    return Text(
-      "${this.count}",
-      style: Theme.of(context)
-          .textTheme
-          .headline4
-          ?.copyWith(fontWeight: FontWeight.normal),
-    );
-  }
-
-  List<Widget> _buildContentWithoutIcon(context) {
-    return [
-      _buildTitle(context),
-      kSizedBoxVerticalS,
-      _buildCount(context),
-    ];
-  }
-
-  List<Widget> _buildContentWithIcon(context) {
-    return [
-      this.icon,
-      kSizedBoxVerticalXS,
-      _buildTitle(context),
-      kSizedBoxVerticalXS,
-      _buildCount(context),
-    ];
-  }
-
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: EdgeInsets.all(kSizeS),
+        padding: const EdgeInsets.all(kSizeS),
         child: Column(
-          children: this.icon.icon == null
-              ? _buildContentWithoutIcon(context)
-              : _buildContentWithIcon(context),
+          children: [
+            if (icon.icon != null)
+              Column(
+                children: [
+                  icon,
+                  kSizedBoxVerticalXS,
+                ],
+              ),
+            Text(
+              title,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              "$count",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline4
+                  ?.copyWith(fontWeight: FontWeight.normal),
+            )
+          ],
         ),
       ),
     );
