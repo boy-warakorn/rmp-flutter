a30d1f4a32fedbcad2bb664c959e9d010a2f473b
Noppanut <noppanut.gapsb2536@mail.kmutt.ac.th>
2021-09-13 21:52:12 +0700

    UPDATE - now has start and end sizedbox

--- lib/widgets/layout/scrollable_card_row.dart
+++ lib/widgets/layout/scrollable_card_row.dart
@@ -3,12 +3,16 @@ import 'package:rmp_flutter/configs/constants.dart';
 
 class ScollableCardRow extends StatelessWidget {
   final SizedBox spacingSizedBox;
+  final SizedBox startSizedBox;
+  final SizedBox endSizedBox;
   final List<Widget> cards;
 
   const ScollableCardRow({
     Key? key,
     this.spacingSizedBox = kSizedBoxHorizontalS,
     this.cards = const [],
+    this.startSizedBox = kSizedBoxHorizontalS,
+    this.endSizedBox = kSizedBoxHorizontalS,
   }) : super(key: key);
 
   List<Widget> _buildCards(){
@@ -16,12 +20,17 @@ class ScollableCardRow extends StatelessWidget {
       return [];
     }else{
       var out = <Widget>[];
+
+      out.add(startSizedBox);
+
       for(int i = 0; i < cards.length; i++){
         out.add(cards[i]);
         if(i != cards.length - 1){
           out.add(spacingSizedBox);
         }
       }
+
+      out.add(endSizedBox);
 
       return out;
     }
