740b0a3676481ad92eebc814f7afc70946e8e0f9
Warakorn <boydoubleb@gmail.com>
2021-09-13 11:50:24 +0700

    refactor: how code struct and padding

--- lib/screens/condos/postal/postal_screen.dart
+++ lib/screens/condos/postal/postal_screen.dart
@@ -12,40 +12,38 @@ class PostalScreen extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Expanded(
-          flex: 1,
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: <Color>[kBrandColor, kBrandAlternativeDarkerColor],
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                kSizedBoxVerticalXS,
-                kSizedBoxVerticalS,
-                Container(
-                  margin: EdgeInsets.only(
-                    left: kSizeM,
-                  ),
-                  child: Text(
-                    "Postal Management",
-                    style: Theme.of(context).textTheme.headline3?.copyWith(
-                          color: kLightColor,
-                        ),
-                  ),
-                ),
+        Container(
+          padding: EdgeInsets.all(
+            kSizeS * 1.5,
+          ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[
+                kBrandColor,
+                kBrandAlternativeDarkerColor,
               ],
             ),
           ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Postal Management",
+                style: Theme.of(context).textTheme.headline3?.copyWith(
+                      color: kLightColor,
+                    ),
+              ),
+            ],
+          ),
         ),
         Expanded(
-          flex: 6,
           child: Container(
+            padding: EdgeInsets.all(
+              kSizeS * 1.5,
+            ),
             decoration: BoxDecoration(
               color: kBgColor,
               borderRadius: BorderRadius.vertical(
@@ -57,74 +55,36 @@ class PostalScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                kSizedBoxVerticalXS,
-                kSizedBoxVerticalS,
-                Container(
-                  margin: EdgeInsets.only(
-                    left: kSizeM,
-                  ),
-                  child: Text(
-                    "All Packages",
-                    style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                          fontSize: kFontSizeHeadline3,
-                          color: kBrandDarkerColor,
-                          fontWeight: FontWeight.bold,
-                        ),
-                  ),
+                Text(
+                  "All Packages",
+                  style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                        fontSize: kFontSizeHeadline3,
+                        color: kBrandDarkerColor,
+                        fontWeight: FontWeight.bold,
+                      ),
                 ),
                 kSizedBoxVerticalXS,
                 kSizedBoxVerticalS,
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: kSizeS,
-                  ),
-                  child: PackageCard(
-                    title: "Gaming Monitor",
-                    date: "13/9/2021",
-                    note: "24 inches monitor",
-                    onPressed: () => print('this is package.'),
-                  ),
+                PackageCard(
+                  title: "Gaming Monitor",
+                  date: "13/9/2021",
+                  note: "24 inches monitor",
+                  onPressed: () => print('this is package.'),
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: kSizeS,
-                  ),
-                  child: PackageCard(
-                    title: "Gaming Monitor",
-                    date: "13/9/2021",
-                    note: "24 inches monitor",
-                    onPressed: () => print('this is package.'),
-                  ),
+                PackageCard(
+                  title: "Gaming Monitor",
+                  date: "13/9/2021",
+                  note: "24 inches monitor",
+                  onPressed: () => print('this is package.'),
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: kSizeS,
-                  ),
-                  child: PackageCard(
-                    title: "Gaming Monitor",
-                    date: "13/9/2021",
-                    note: "24 inches monitor",
-                    onPressed: () => print('this is package.'),
-                  ),
+                PackageCard(
+                  title: "Gaming Monitor",
+                  date: "13/9/2021",
+                  note: "24 inches monitor",
+                  onPressed: () => print('this is package.'),
                 ),
               ],
             ),
-          ),
-        ),
-        Container(
-          alignment: Alignment.bottomRight,
-          margin: EdgeInsets.only(
-            right: kSizeS,
-            bottom: kSizeS,
-          ),
-          child: FloatingActionButton(
-            elevation: 10,
-            onPressed: () => print('onPressed'),
-            child: Icon(
-              Icons.add,
-              size: kSizeM,
-            ),
-            backgroundColor: kBrandColor,
           ),
         ),
       ],
