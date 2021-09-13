164567f157af9bb413d405042f467a4ffa549f8a
Noppanut <noppanut.gapsb2536@mail.kmutt.ac.th>
2021-09-13 15:06:14 +0700

    ADD - HelpDeskScreen

--- lib/screens/condos/help-desk/help_desk_screen.dart
+++ lib/screens/condos/help-desk/help_desk_screen.dart
@@ -1,10 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:rmp_flutter/configs/constants.dart';
+import 'package:rmp_flutter/widgets/general/custom_button.dart';
+import 'package:rmp_flutter/widgets/general/custom_slider.dart';
+import 'package:rmp_flutter/widgets/general/help_desk_card.dart';
 
-class HelpDeskScreen extends StatelessWidget {
+const loremIpsum =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare accumsan nulla non accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam eget pharetra lacus. Maecenas et dolor blandit, sodales justo pharetra,";
+
+const dummy = [
+  {
+    "title": "T1",
+    "date": "20/20/2202",
+    "detail": loremIpsum,
+    "isResponded": true,
+  },
+  {
+    "title": "T2",
+    "date": "20/20/2202",
+    "detail": loremIpsum,
+    "isResponded": false,
+  },
+  {
+    "title": "T3",
+    "date": "20/20/2202",
+    "detail": loremIpsum,
+    "isResponded": true,
+  },
+];
+
+class HelpDeskScreen extends HookWidget {
+  static const routeName = "/condo/helpdesk";
+
   const HelpDeskScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Welcome HelpDesk'));
+    final _isResponded = useState(true);
+
+    void switchResponded(dynamic to) {
+      _isResponded.value = to;
+    }
+
+    List<Map<String, dynamic>> filterList() {
+      return dummy
+          .where((ele) => ele["isResponded"] == _isResponded.value)
+          .toList();
+    }
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: kSizeS * (24 / 16),
+        ),
+        child: Column(
+          children: [
+            kSizedBoxVerticalS,
+            kSizedBoxVerticalXS,
+            CustomSlider(
+              isResponded: _isResponded.value,
+              onValueChanged: switchResponded,
+            ),
+            kSizedBoxVerticalS,
+            kSizedBoxHorizontalXS,
+            Expanded(
+              child: ListView.builder(
+                itemCount: filterList().length,
+                itemBuilder: (context, index) => HelpDeskCard(
+                  title: "${filterList()[index]["title"]}",
+                  date: "${filterList()[index]["date"]}",
+                  detail: "${filterList()[index]["detail"]}",
+                  actionButton: filterList()[index]["isResponded"] as bool
+                      ? CustomButton(
+                          onPressed: () => print("Reply"),
+                          text: "Reply",
+                        )
+                      : const SizedBox(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
