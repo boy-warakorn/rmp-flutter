cdddfa19f3ad5fd325047cf7da2510ab76106524
Warakorn <boydoubleb@gmail.com>
2021-09-17 16:20:52 +0700

    chore: clean up

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rmp_flutter/configs/routes.dart';
 import 'package:rmp_flutter/configs/style.dart';
-import 'package:rmp_flutter/models/providers/user_provider.dart';
-import 'package:rmp_flutter/screens/login_screen.dart';
-import 'package:rmp_flutter/screens/main_screen.dart';
 import 'package:rmp_flutter/screens/preloading_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
