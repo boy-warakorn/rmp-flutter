2e6f3f370b6ec813fa93bc716a1b11d81d967a02
Onnopz <47559485+Onnopz@users.noreply.github.com>
2021-11-16 20:33:25 +0700

    [UPDATE] apply TextTab and switch incoming items to the first tab

--- lib/screens/condos/help-desk/help_desk_screen.dart
+++ lib/screens/condos/help-desk/help_desk_screen.dart
@@ -8,7 +8,7 @@ import 'package:rmp_flutter/screens/condos/help-desk/reply_screen.dart';
 import 'package:rmp_flutter/widgets/general/help_desk_card.dart';
 import 'package:rmp_flutter/utils/date_format.dart';
 import 'package:rmp_flutter/widgets/interactions/custom_button.dart';
-import 'package:rmp_flutter/widgets/interactions/custom_slider.dart';
+import 'package:rmp_flutter/widgets/interactions/text_tab.dart';
 
 class HelpDeskScreen extends HookConsumerWidget {
   static const routeName = "/condo/helpdesk";
@@ -18,6 +18,7 @@ class HelpDeskScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final _isResponded = useState(true);
+    final _tabIndex = useState(0);
 
     final _reports = useState(ReportsModel(reports: []));
     final _isLoading = useState(false);
@@ -25,68 +26,72 @@ class HelpDeskScreen extends HookConsumerWidget {
     void fetchReports() async {
       _isLoading.value = true;
       _reports.value =
-          await ReportRepository().getReportsByCondo(_isResponded.value);
+          await ReportRepository().getReportsByCondo(_tabIndex.value == 1);
       _isLoading.value = false;
-    }
-
-    useEffect(() {
-      fetchReports();
-    }, []);
-
-    void switchResponded(bool switchTo) {
-      _isResponded.value = switchTo;
     }
 
     useEffect(
       () {
         fetchReports();
       },
-      [_isResponded.value],
+      [_tabIndex.value],
     );
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: kSizeS * (24 / 16),
-      ),
-      child: Column(
-        children: [
-          kSizedBoxVerticalS,
-          kSizedBoxVerticalXS,
-          CustomSlider(
-            isResponded: _isResponded.value,
-            onValueChanged: (switchTo) => switchResponded(switchTo),
-          ),
-          kSizedBoxVerticalS,
-          kSizedBoxHorizontalXS,
-          _isLoading.value
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Expanded(
-                  child: ListView.builder(
-                    itemCount: _reports.value.reports.length,
-                    itemBuilder: (context, index) {
-                      final _currentReport = _reports.value.reports[index];
-                      return HelpDeskCard(
-                        owner: _currentReport.reportOwner,
-                        title: _currentReport.title,
-                        date: formattedDate(_currentReport.requestedDate),
-                        detail: _currentReport.detail,
-                        actionButton: CustomButton(
-                          onPressed: () {
-                            Navigator.of(context)
-                                .pushNamed(ReplyScreen.routeName,
-                                    arguments: _currentReport.id)
-                                .then((value) => fetchReports());
+    return Column(
+      children: [
+        TextTab(
+          labels: [
+            "Incoming",
+            "Responded",
+          ],
+          selectedIndex: _tabIndex.value,
+          onSelect: (i) {
+            _tabIndex.value = i;
+          },
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: kSizeS * (24 / 16),
+              vertical: kSizeS * (24 / 16),
+            ),
+            child: Column(
+              children: [
+                _isLoading.value
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : Expanded(
+                        child: ListView.builder(
+                          itemCount: _reports.value.reports.length,
+                          itemBuilder: (context, index) {
+                            final _currentReport =
+                                _reports.value.reports[index];
+                            return HelpDeskCard(
+                              owner: _currentReport.reportOwner,
+                              title: _currentReport.title,
+                              date: formattedDate(_currentReport.requestedDate),
+                              detail: _currentReport.detail,
+                              actionButton: CustomButton(
+                                onPressed: () {
+                                  Navigator.of(context)
+                                      .pushNamed(ReplyScreen.routeName,
+                                          arguments: _currentReport.id)
+                                      .then((value) => fetchReports());
+                                },
+                                text: !_isResponded.value
+                                    ? "Reply"
+                                    : "See detail",
+                              ),
+                            );
                           },
-                          text: !_isResponded.value ? "Reply" : "See detail",
                         ),
-                      );
-                    },
-                  ),
-                ),
-        ],
-      ),
+                      ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
